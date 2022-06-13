0554c7bcd74d4f966f352d37b71d8a9dd83e77cb
Firstname Lastname <your_email@youremail.com>
2022-06-13 02:59:39 +0100

    gg

--- config/database.php
+++ config/database.php
@@ -36,17 +36,8 @@ return [
     'connections' => [
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'sara-pfe'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-            'options' => [
-                // here you can pass more settings to the Mongo Driver Manager
-                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-            ],
+            'dsn' => env('DB_URI'),
+            'database' => 'sata-pfe',
         ],
         'sqlite' => [
             'driver' => 'sqlite',
